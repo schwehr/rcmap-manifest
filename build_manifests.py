e7f3513e06ca9ec76376437ab3988be7aa217cfa
Kurt Schwehr <schwehr@google.com>
2023-03-20 10:57:36 -0700

    Cleanup: year and trend in name uppercase, uris should have an array with a single path, remove extra comments

--- build_manifests.py
+++ build_manifests.py
@@ -6,87 +6,15 @@ USGS RCMAP Manifest generation.
 
 gs://ee-nlcd-upload
 
-var dataset = ee.ImageCollection('projects/ee-rcmap/assets/RCMAP_V5_TRENDS/RCMAP_V5_TRENDS');
+var trends = ee.Image('projects/ee-rcmap/assets/RCMAP_V5_TRENDS/TRENDS');
+var dataset = ee.ImageCollection('projects/ee-rcmap/assets/RCMAP_V5_TRENDS/year');
 
 print(dataset);
-
-What needs MODE?
-break_point
-
-Name - Trends (8 areas)      | nodata value
-bp_count                     | 101 and 4
-pvalue_linear                | 102
-slope_linear                 | -32768
-total_change_intensity_index | 101
-year_recent_break            | 0
-bp_year  (yearly)            | None (leave blank)
-pvalue_seg  (yearly)         | 102
-slope_seg  (yearly)          | -32768
-
-Name - Components (9) | 101
 """
 import json
 
 BASE_ID = 'projects/ee-rcmap/assets/RCMAP_V5_TRENDS/'
 BUCKET = 'gs://ee-nlcd-upload/rcmap_'
-
-
-
-# grep -v '_[0-9]' files.txt  | cut -d\  -f4-
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_break_point.tif
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_most_recent_break_point.tif
-# gs://ee-nlcd-upload/rcmap_bare_ground_break_point.tif
-# gs://ee-nlcd-upload/rcmap_bare_ground_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_bare_ground_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_bare_ground_most_recent_break_point.tif
-# gs://ee-nlcd-upload/rcmap_herbaceous_break_point.tif
-# gs://ee-nlcd-upload/rcmap_herbaceous_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_herbaceous_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_herbaceous_most_recent_break_point.tif
-# gs://ee-nlcd-upload/rcmap_litter_break_point.tif
-# gs://ee-nlcd-upload/rcmap_litter_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_litter_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_litter_most_recent_break_point.tif
-# gs://ee-nlcd-upload/rcmap_non_sagebrush_shrub_break_point.tif
-# gs://ee-nlcd-upload/rcmap_non_sagebrush_shrub_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_non_sagebrush_shrub_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_non_sagebrush_shrub_most_recent_break_point.tif
-# gs://ee-nlcd-upload/rcmap_perennial_herbaceous_break_point.tif
-# gs://ee-nlcd-upload/rcmap_perennial_herbaceous_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_perennial_herbaceous_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_perennial_herbaceous_most_recent_break_point.tif
-# gs://ee-nlcd-upload/rcmap_sagebrush_break_point.tif
-# gs://ee-nlcd-upload/rcmap_sagebrush_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_sagebrush_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_sagebrush_most_recent_break_point.tif
-# gs://ee-nlcd-upload/rcmap_shrub_break_point.tif
-# gs://ee-nlcd-upload/rcmap_shrub_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_shrub_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_shrub_most_recent_break_point.tif
-# gs://ee-nlcd-upload/rcmap_total_change_intensity_index.tif
-# gs://ee-nlcd-upload/rcmap_tree_break_point.tif
-# gs://ee-nlcd-upload/rcmap_tree_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_tree_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_tree_most_recent_break_point.tif
-
-
-# bp_count 101 and 4 - 8bit unsigned - MODE
-# bp_year none - 1bit unsigned - MODE
-# pvalue_linear 102 - 8bit unsigned - mean
-# pvalue_seg 102 - 8bit unsigned - mean
-# slope_linear -32768 - 16bit signed - mean
-# slope_seg -32768 - 16bit signed -
-# total_change_intensity_index 101 - 8bit unsigned - mean
-# year_recent_break 0 16bit unsigned MODE
-
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_break_point.tif
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_linear_model_pvalue.tif
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_linear_model_slope.tif
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_most_recent_break_point.tif
-
-# gs://ee-nlcd-upload/rcmap_total_change_intensity_index.tif
 
 MODE = 'mode'
 MEAN = 'mean'
@@ -96,12 +24,9 @@ STATS_NON_YEAR = [
     ['linear_model_slope', MEAN],
     ['linear_model_pvalue', MEAN],
     ['most_recent_break_point', MODE]]
-# total_change_intensity_index is the band name too.
-# MEAN
+# total_change_intensity_index is the band name too - MEAN
 TOTAL_CHANGE_INTENSITY_INDEX = 'total_change_intensity_index'
 
-# gs://ee-nlcd-upload/rcmap_tree_break_point.tif
-# gs://ee-nlcd-upload/rcmap_tree_linear_model_pvalue.tif
 
 def trends() -> dict[str, object]:
   """Returns a manifest structure."""
@@ -121,11 +46,7 @@ def trends() -> dict[str, object]:
   for stat, averaging in STATS_NON_YEAR:
     for land in LAND_TYPES:
       name = f'{land}_{stat}'
-      entry = {
-          'id': name,
-          'tilesetId': name,
-          # TODO(schwehr): Needed? 'missingData': {'values': [] }
-          }
+      entry = {'id': name, 'tilesetId': name}
       if averaging == MODE:
         entry['pyramidingPolicy'] = MODE
       bands.append(entry)
@@ -136,7 +57,7 @@ def trends() -> dict[str, object]:
   })
 
   result = {
-      'name': BASE_ID + 'trends',
+      'name': BASE_ID + 'TRENDS',
       'tilesets': tilesets,
       'bands': bands,
       'startTime': '1985-01-01T00:00:00Z',
@@ -145,17 +66,13 @@ def trends() -> dict[str, object]:
 
   return result
 
-# gs://ee-nlcd-upload/rcmap_annual_herbaceous_segment_slope_2021.tif"
 
 LAND_TYPES = [
     'annual_herbaceous', 'bare_ground', 'herbaceous', 'litter', 'sagebrush',
     'shrub', 'non_sagebrush_shrub', 'perennial_herbaceous', 'tree',
 ]
 
-#  40M 2023/01/03 11:51:46 gs://ee-nlcd-upload/rcmap_shrub_break_point_2021.tif
-
 STAT_TYPES = ['break_point', 'segment_pvalue', 'segment_slope']
-
 
 
 def yearly(year: int) -> dict[str, object]:
@@ -166,26 +83,19 @@ def yearly(year: int) -> dict[str, object]:
   for stat_type in STAT_TYPES:
     for land_type in LAND_TYPES:
       name = f'{land_type}_{stat_type}'
-      # gs://ee-nlcd-upload/rcmap_annual_herbaceous_break_point_2021.tif
-      # gs://ee-nlcd-upload/rcmap_tree_segment_slope_2004.tif
       path = f'{BUCKET}{land_type}_{stat_type}_{year}.tif'
 
-      # TODO is uris an array or single string
-      tilesets.append({'id': name, 'sources': [{'uris': path}]})
+      tilesets.append({'id': name, 'sources': [{'uris': [path]}]})
 
   bands = []
   for stat in STAT_TYPES:
     for land in LAND_TYPES:
       name = f'{land}_{stat}'
-      entry = {
-          'id': name,
-          'tilesetId': name,
-          # TODO(schwehr): Needed? 'missingData': {'values': [] }
-      }
+      entry = {'id': name, 'tilesetId': name}
       bands.append(entry)
 
   result = {
-      'name': f'{BASE_ID}year/{year}',
+      'name': f'{BASE_ID}YEAR/{year}',
       'tilesets': tilesets,
       'bands': bands,
       'startTime': f'{year}-01-01T00:00:00Z',
